0111e7492f21a483ef8c5761784a732b53106188
Branislav Taskovikj <95235265+taskovikj@users.noreply.github.com>
2023-08-29 11:03:35 +0200

    Update settings.py

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -58,8 +58,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'branislav.taskovikj@gmail.com'
-EMAIL_HOST_PASSWORD = 'horvbpojzaerwpcq'
+EMAIL_HOST_USER = 'Enter your email'
+EMAIL_HOST_PASSWORD = 'Enter your password'
 
 
 # if not DEBUG:
