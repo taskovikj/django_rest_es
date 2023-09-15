a22120602276c5158abfd1e1e6d7c48653a4399c
taskovikj <branislav.taskovikj@gmail.com>
2023-09-15 14:38:44 +0200

    Git actions

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from base.models import Items, Book, Author
 
+
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Items
@@ -11,6 +12,7 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
